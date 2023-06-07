77476a1f2126c344c46e1ea18a764f681cbe99f6
asadiiefji <asadiiefji@gmail.com>
2023-06-07 13:57:56 +0700

    Initial Commit

---
+++ WetonKulo/WetonKuloApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetonKuloApp.swift
+//  WetonKulo
+//
+//  Created by Mukhammad Miftakhul As'Adi on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WetonKuloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
